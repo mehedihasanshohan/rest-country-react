469bf3034b6ee7a7afaffd0296c618116e494a34
mehedi hasan <mhshohan04@gmail.com>
2025-09-19 12:03:30 +0600

    conditional text show

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Country = ({country}) => {
 
+
   // console.log(country);
   const {area, capital, flags, name, population, region} = country;
 
@@ -11,8 +12,8 @@ const Country = ({country}) => {
         <h2>Name: <span className='text-rose-300 font-medium'>{name.common}</span> </h2>
         <p>Area: {area.area}</p>
         <p> Capital: <span className='text-blue-300 font-medium'>{capital.capital[0]}</span></p>
-        <p>Population: {population.population}</p>
         <p>Region: {region.region}</p>
+        <p>Population: {population.population} <br /> {population.population > 200000 ? 'A Large Country' : 'A Small Country'}</p>
       </div>
       <div className=''>
         <img className='h-24 w-40 object-cover rounded-2xl' src={flags.flags.svg} alt="" />
