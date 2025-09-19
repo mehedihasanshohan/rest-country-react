0a30745ef8d38e94d917b6c004c942b9d34b4157
mehedi hasan <mhshohan04@gmail.com>
2025-09-19 12:16:53 +0600

    toggle visited button

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Country = ({country}) => {
 
+  const [visited, setVisited] = useState(false);
 
+  const handleVisit = () =>{
+    setVisited(!visited);
+  }
   // console.log(country);
   const {area, capital, flags, name, population, region} = country;
 
   return (
+    <>
     <div className='flex justify-between items-center gap-2 border border-amber-300 rounded-lg mt-4 p-4'>
       <div>
         <h2>Name: <span className='text-rose-300 font-medium'>{name.common}</span> </h2>
@@ -14,11 +19,13 @@ const Country = ({country}) => {
         <p> Capital: <span className='text-blue-300 font-medium'>{capital.capital[0]}</span></p>
         <p>Region: {region.region}</p>
         <p>Population: {population.population} <br /> {population.population > 200000 ? 'A Large Country' : 'A Small Country'}</p>
+        <button onClick={handleVisit} className='mt-2' >{visited? 'Visited' : 'Not Visit yet'}</button>
       </div>
       <div className=''>
         <img className='h-24 w-40 object-cover rounded-2xl' src={flags.flags.svg} alt="" />
       </div>
     </div>
+    </>
   )
 }
 
